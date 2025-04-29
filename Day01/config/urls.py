63b8675ab2ae2b2e72e7bf52fb1505145c1c3c2f
muk <lhm4802@nate.com>
2025-04-29 22:55:29 +0900

    ✨ feat: 유저 템플릿 연결 및 리스트 출력 완료

--- Day01/config/urls.py
+++ Day01/config/urls.py
@@ -37,9 +37,3 @@ urlpatterns = [
     path('users/<int:user_id>/', user_info, name='user_info'),
     path('admin/', admin.site.urls),
 ]
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
