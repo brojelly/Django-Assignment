46181fba57f60303f81984be656b223bb3a61fec
muk <lhm4802@nate.com>
2025-04-29 21:03:28 +0900

    ✨ feat: users url/view 연결 및 fake_db 연동

--- Day01/config/urls.py
+++ Day01/config/urls.py
@@ -15,6 +15,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import Http404
+from django.shortcuts import render
+from django.urls import path
+from fake_db import user_db
+
+_db = user_db
+
+def user_list(request):
+    names = [{'id': key, 'name': value['이름']} for key, value in _db.items()]
+    return render(request, 'user_list.html', {'data': names})
+
+def user_info(request, user_id):
+    if user_id not in _db:
+        raise Http404('User not found')
+    info = _db[user_id]
+    return render(request, 'user_info.html', {'data': info})
+
+urlpatterns = [
+    path('users/', user_list, name='user_list'),
+    path('users/<int:user_id>/', user_info, name='user_info'),
+    path('admin/', admin.site.urls),
+]
+from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
